fdeb847af4483dad6fafb7a2570f16654e5a5840
Diego Reis <diegoreso@me.com>
2025-05-16 17:13:42 -0300

    Project and Book entity created

---
+++ src/main/java/com/reso/libraryapi/model/Book.java
@@ -0,0 +1,104 @@
+package com.reso.libraryapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private LocalDate publicationDate;
+    private String publisher;
+    private Details details;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String isbn, String author, LocalDate publicationDate, String publisher, Details details) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+        this.publicationDate = publicationDate;
+        this.publisher = publisher;
+        this.details = details;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Details getDetails() {
+        return details;
+    }
+
+    public void setDetails(Details details) {
+        this.details = details;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/reso/libraryapi/model/Details.java
@@ -0,0 +1,10 @@
+package com.reso.libraryapi.model;
+
+import jakarta.persistence.*;
+
+@Embeddable
+public class Details {
+
+
+
+}
